f8aac761b8c4d4854abe48ccc1a2c41c072f8476
ilthraim <mobillegos@gmail.com>
2020-07-04 17:47:06 -0400

    new gpu config and updated i3 status bar with cool colors

--- .scripts/i3status_optimus.sh
+++ .scripts/i3status_optimus.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-i3status | while :
-do
-	read line
-	gpu=$(optimus-manager --print-mode | awk '{print $NF}')
-	echo "$gpu | $line" || exit 1
-done
+gpu=$(optimus-manager --print-mode | awk '{print $NF}')
+gpu=${gpu#?}
+
+if [ "$gpu" = "intel" ]; then
+	printf "$gpu\n#0088ff"
+elif [ "$gpu" = "hybrid" ]; then
+	printf "$gpu\n#FF0000"
+else
+	printf "$gpu\n#00ff00"
+fi
